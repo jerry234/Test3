d9988b7510e0e75aa4a5d08b2f1b9afa143a5cc5
Adrian <acubak90@gmail.com>
2019-08-15 19:11:32 +0200

    1cx

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello Wcxczxcxcorld!");
         System.out.println("Elo320");
     }
 }
